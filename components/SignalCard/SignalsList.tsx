ced943157bff60a9a303f8f4b65f64691c74b083
asd12288 <ilanchelly@gmail.com>
2025-02-15 18:01:42 +0200

    prefrences check

--- components/SignalCard/SignalsList.tsx
+++ components/SignalCard/SignalsList.tsx
@@ -9,15 +9,17 @@ import useProfile from "@/hooks/useProfile";
 
 const SignalsList = ({ userId }) => {
   const { signals, isLoading } = useSignals();
-
   const { isLoading: isLoadingProfile, profile } = useProfile(userId);
 
-  if (isLoadingProfile) {
+  if (isLoading || isLoadingProfile) {
     return <LoaderCards />;
   }
 
-  const favoritePreferences = Object.entries(profile.preferences || {})
-    .filter(([key, value]) => value.favorite)
+  // Add null checks and default values
+  const preferences = profile?.preferences || {};
+
+  const favoritePreferences = Object.entries(preferences)
+    .filter(([_, value]) => value?.favorite)
     .reduce((acc, [key, value]) => {
       acc[key] = value;
       return acc;
@@ -31,36 +33,23 @@ const SignalsList = ({ userId }) => {
 
   return (
     <div>
-      {isLoading ? (
-        <LoaderCards />
-      ) : (
-        <>
-          {favouriteInstruments.length > 0 && (
-            <FavoriteSignals
-              favouriteSignals={favouriteSignals}
-              preferences={profile.preferences}
-            />
-          )}
-
-          <div className="grid grid-cols-3 gap-8">
-            {signals.map((signal: any, index: number) => (
-              <Link
-                key={
-                  signal.trade_client_id
-                    ? `${signal.trade_client_id}-${signal.instrument_name}-${index}`
-                    : `${signal.instrument_name}-${index}`
-                }
-                href={`/signals/${signal.instrument_name}`}
-              >
-                <SignalCard
-                  signalPassed={signal}
-                  preferences={profile.preferences}
-                />
-              </Link>
-            ))}
-          </div>
-        </>
+      {favouriteInstruments.length > 0 && (
+        <FavoriteSignals
+          favouriteSignals={favouriteSignals}
+          preferences={preferences}
+        />
       )}
+
+      <div className="grid grid-cols-3 gap-8">
+        {signals.map((signal: any, index: number) => (
+          <Link
+            key={`${signal.instrument_name}-${index}`}
+            href={`/signals/${signal.instrument_name}`}
+          >
+            <SignalCard signalPassed={signal} preferences={preferences} />
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
